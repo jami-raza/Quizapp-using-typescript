fe81e769e5bef632286a157e58cc66fb8acebaba
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-08-31 23:37:52 +0500

    Revert "project in progress"
    This reverts commit 51b3d07b653e932f42f71eab30049bc716ba127a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import Home from './Components/Home';
-function App(){
-  return(
-    <div>
-      <Home/>
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      Lets Start Quiz app</h1>
     </div>
-  )
+  );
 }
+
 export default App;
