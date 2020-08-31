7c00b762a92f836851c08a1a45249a67c995f0cf
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-09-01 00:13:08 +0500

    update app

---
+++ src/Types/Quiz_Types.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export type QuestionType ={
+    category: string
+    correct_answer: string
+    difficulty: string
+    incorrect_answers: string[]
+    question: string
+    type: string
+
+}
+
+export type Quiz_type={
+    question:string
+    answer:string
+    options:string[]
+    correct_answer:string
+}
+export type Quiz_end={
+    score:number
+    
+}
+
